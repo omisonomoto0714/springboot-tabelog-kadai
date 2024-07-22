23a1f0bc2ac702f2c07ebd17a0ab03524ac45628
omisonomoto0714 <omisonomoto0618@gmail.com>
2024-07-22 11:34:24 +0900

    エラー

--- nagoyamesi/src/main/java/com/example/nagoyamesi/controller/PasswordResetController.java
+++ nagoyamesi/src/main/java/com/example/nagoyamesi/controller/PasswordResetController.java
@@ -2,11 +2,16 @@ package com.example.nagoyamesi.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.nagoyamesi.entity.User;
 import com.example.nagoyamesi.form.PasswordResetForm;
 import com.example.nagoyamesi.repository.UserRepository;
 import com.example.nagoyamesi.service.UserService;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class PasswordResetController {
@@ -25,4 +30,23 @@ public class PasswordResetController {
     }
 	
 
+    @PostMapping("/passwordReset")
+    public String handlePasswordReset(@Valid PasswordResetForm form, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "auth/passwordReset";
+        }
+
+        // ユーザーの検索
+        User user = userRepository.findByEmail(form.getEmail());
+        if (user == null) {
+            result.rejectValue("email", "error.passwordResetForm", "該当するメールアドレスが見つかりません。");
+            return "auth/passwordReset";
+        }
+
+        // パスワード更新
+        userService.updatePassword(user, form.getPassword());
+
+        model.addAttribute("message", "パスワードが正常に更新されました。");
+        return "redirect:/login?updateSuccess";
+    }
 }
