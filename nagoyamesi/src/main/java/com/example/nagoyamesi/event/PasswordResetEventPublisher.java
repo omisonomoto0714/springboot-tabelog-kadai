43d30d883059256f0efe77101a29cb1d835d02a4
omisonomoto0714 <omisonomoto0618@gmail.com>
2024-07-29 23:58:33 +0900

    追加

---
+++ nagoyamesi/src/main/java/com/example/nagoyamesi/event/PasswordResetEventPublisher.java
@@ -0,0 +1,22 @@
+package com.example.nagoyamesi.event;
+
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Component;
+
+import com.example.nagoyamesi.entity.User;
+
+@Component
+public class PasswordResetEventPublisher {
+	private final ApplicationEventPublisher applicationEventPublisher;
+
+	public PasswordResetEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+		this.applicationEventPublisher = applicationEventPublisher;
+	}
+
+	public void publishPasswordResetEvent(User user, String requestUrl) {
+		applicationEventPublisher.publishEvent(new PasswordResetEvent(this, user, requestUrl));
+	}
+//	public void publishSubscriptionEvent(User user, String requestUrl) {
+//		applicationEventPublisher.publishEvent(new PasswordResetEvent(this, user, requestUrl));
+//	}
+}
